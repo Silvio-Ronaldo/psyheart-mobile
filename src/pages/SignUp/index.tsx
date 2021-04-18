885ed4a12063928e70c1af33a5613ac3957b18b7
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-04-17 22:07:06 -0300

    Criando pagina de cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,65 @@
 import React from 'react';
+import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+  Container,
+  Brand,
+  Logo,
+  Name,
+  Icon,
+  Title,
+  BackToSignIn,
+  BackToSignInText,
+} from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Brand>
+              <Logo>
+                <Name>PsyHeart</Name>
+              </Logo>
+              <Icon name="heart" size={80} color="#fb1528" />
+            </Brand>
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('Oi');
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => {}}>
+        <Icon name="arrow-left" size={20} color="#ffffff" />
+        <BackToSignInText>Voltar para login</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
 };
 
 export default SignUp;
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,3 +1,62 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+`;
+
+export const Brand = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Logo = styled.View`
+  background-color: #fb1528;
+
+  padding: 10px 16px;
+  border-radius: 10px;
+`;
+
+export const Name = styled.Text`
+  color: #ffffff;
+  font-size: 40px;
+  font-family: 'Poppins-Medium';
+`;
+
+export const Icon = styled(FeatherIcon)`
+  margin-left: 10px;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #ffffff;
+  font-family: 'Poppins-Medium';
+  margin: 30px 0 24px;
+`;
+
+export const BackToSignIn = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #232129;
+  border-top-width: 1px;
+  border-color: #fb1528;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const BackToSignInText = styled.Text`
+  color: #ffffff;
+  font-size: 18px;
+  font-family: 'Poppins-Regular';
+  margin-left: 16px;
+`;
